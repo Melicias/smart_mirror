0a8cab66cb11f6f6551feab33382c7d1af33300e
Francisco Melicias <melicias1999@gmail.com>
2019-11-25 14:07:29 +0000

    Update config.py

--- classes/configs/config.py
+++ classes/configs/config.py
@@ -1,7 +1,7 @@
-weather_api = {'key': 'cd38b793c29e32a02798494f81e4df0c',
+weather_api = {'key': '',
 			   'city_id': 2267095}
 
-news_api = {'key': '5770ad4f261349b886ca4a187e593fb9',
+news_api = {'key': '',
          	'country': 'us',
          	'language': 'en',
          	'category': 'technology',
